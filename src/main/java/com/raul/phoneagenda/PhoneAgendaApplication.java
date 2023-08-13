fb437175248d0444e3260149008c422258305ba7
unknown <rosa_raul2000@yahoo.com>
2023-08-13 09:39:10 +0300

    first commit

---
+++ src/main/java/com/raul/phoneagenda/PhoneAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.raul.phoneagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raul/phoneagenda/PhoneAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.phoneagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
